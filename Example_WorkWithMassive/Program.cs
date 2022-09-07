0e949930b0b8de87aedc4f841be2c60f542bf085
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-07 21:28:39 +0300

    add ex Work With Massive

---
+++ Example_WorkWithMassive/Program.cs
@@ -0,0 +1,68 @@
+﻿// Сортировка методом выбора (от минимального к максимальному)
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) // выводит на печать наш массив
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)     // упорядочивание от минимального к максимальному
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
+
+// Сортировка методом выбора (от максимального к минимальному)
+int[] arr1 = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray1(int[] array) // выводит на печать наш массив
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort1(int[] array)     // упорядочивание от максимума
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr1);
+SelectionSort1(arr1);
+
+PrintArray(arr1);
